fc499ec367074f47b4e6ac478cb9a8c54f8b71ec
Bootla Saikrishna <saikrishna>
2024-09-13 15:28:55 +0530

    commiting V1-EC2.tf

---
+++ terraform_code/V1-EC2.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0a5c3558529277641"
+    instance_type = "t2.micro"
+    key_name = "sep12"
+    security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+
+  tags = {
+    Name = "demo-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
